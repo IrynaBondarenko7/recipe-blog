7701619fd353ff7d07d558dfee3f81d44f990f9c
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-10-27 16:15:31 +0000

    add simple routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { HomeComponent } from './pages/home/home.component';
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'terms-contidions', component: TermsAndConditionsComponent },
+];
