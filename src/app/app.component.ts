cdccff9bc56fb471cef8ceba956db5398a96e79f
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-11-01 15:33:37 +0000

    design subscription form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PostCardComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    PostCardComponent,
+    SubscriptionFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
