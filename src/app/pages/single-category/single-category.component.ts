57e1f5b7109f88238b136c2700f252a091d1d7da
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-11-29 13:57:45 +0000

    design single category page

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 
 @Component({
   selector: 'app-single-category',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './single-category.component.html',
-  styleUrl: './single-category.component.css'
+  styleUrl: './single-category.component.css',
 })
-export class SingleCategoryComponent {
-
-}
+export class SingleCategoryComponent {}
