21e6451d2fc95f2226b3b68edf4fda442da58b13
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-05-04 20:15:38 +0100

    updated categories service because of implementing soft delete principle for categories

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  query,
+  where,
+} from '@angular/fire/firestore';
 import { Category } from '../models/category';
 
 @Injectable({
@@ -10,7 +16,10 @@ export class CategoriesService {
   constructor(private firestore: Firestore) {}
 
   loadData(): Observable<Category[]> {
-    const categoriesCollection = collection(this.firestore, 'categories');
+    const categoriesCollection = query(
+      collection(this.firestore, 'categories'),
+      where('isDeleted', '==', false)
+    );
     return collectionData(categoriesCollection, {
       idField: 'id',
     }) as Observable<Category[]>;
